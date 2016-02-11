d1eb89957f370eac2dfc2c874451b57e31b8a544
Steve Cardozo <cardos@rpi.edu>
2016-02-11 01:44:39 -0500

    Tweets Properly Scroll
    Basic functionality done

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Web Science Systems Development: Lab 1
 To start off my index.html file, I used the template provided on http://getbootstrap.com/getting-started/
 
 I found out I could use jQuery's getJSON method to perform an AJAX request to read the JSON file for the tweets. I used the template provided at http://api.jquery.com/jquery.getjson/ to structure the method call (more specifically, the .done and .fail parts).
+
+chose utc to show timezone, in case people from around world
+the times seem to be all the same, but if this applicatoin was used in the real world, they would be different
--- js/displayMessages.js
+++ js/displayMessages.js
@@ -3,6 +3,7 @@ $(function() {
 	var times = [];
 	var pictures = [];
 	var names = [];
+	var screennames = [];	
 	var messages = [];
 	
 	// get data from the JSON file
@@ -10,13 +11,39 @@ $(function() {
    		// called if the file was found
    		.done(function (data) {
    			console.log( "JSON Success!" );
+
    			// go through each tweet and push the text onto the messages array
    			$.each(data, function(tweetNumber, tweet) {
    				times.push(tweet.created_at);
    				pictures.push(tweet.user.profile_image_url);
    				names.push(tweet.user.name);
+   				screennames.push(tweet.user.screen_name);
    				messages.push(tweet.text);
-   			})
+   			});
+
+   			// keep track of the current tweet with a counter variable
+   			var counter = 0;
+
+   			// display the first 5 tweets
+   			while (counter < 5) {
+   				$("#time").prepend("<section class=img-rounded><time>" + new Date(times[counter]).toUTCString() + "</time></section>");
+		  		$("#user").prepend("<section class=img-rounded><img class=img-rounded class=img-responsive src=" + pictures[counter] + "><div><h5><b>" + names[counter] + "</b></h5><br><h6>@" + screennames[counter] + "</h6></div></section>");
+		  		$("#message").prepend("<section class=img-rounded><p>" + messages[counter] + "</p></section>");
+		  		counter++;
+   			}
+
+   			// scroll through the tweets every 3 seconds   			
+			window.setInterval(function(){
+				// remove the last tweet
+				$("#time section:last").remove();
+				$("#user section:last").remove();
+				$("#message section:last").remove();
+				// add a new tweet to the top
+		  		$("#time").prepend("<section class=img-rounded><time>" + new Date(times[counter]).toUTCString() + "</time></section>");
+		  		$("#user").prepend("<section class=img-rounded><img class=img-rounded class=img-responsive src=" + pictures[counter] + "><div><h5><b>" + names[counter] + "</b></h5><br><h6>@" + screennames[counter] + "</h6></div></section>");
+		  		$("#message").prepend("<section class=img-rounded><p>" + messages[counter] + "</p></section>");
+		  		counter++;		  		
+			}, 3000);
    		})
    		// called if the file was not found
    		.fail(function( jqxhr, textStatus, error ) {

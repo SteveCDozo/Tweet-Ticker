93c29487fc22daf3ac1187fa32430fd70bdb2418
Steve Cardozo <cardos@rpi.edu>
2016-02-10 21:26:11 -0500

    Stored messages in an array
    The messages can be accessed from an array now

--- js/displayMessages.js
+++ js/displayMessages.js
@@ -1,13 +1,21 @@
 $(function() {
 	// the tweet messages will be stored in an array
 	var messages = [];
-
+	
+	// get data from the JSON file
    $.getJSON('./tweetsFromTwitter.json')
+   		// called if the file was found
    		.done(function (data) {
-   			console.log( "Success!" );
+   			console.log( "JSON Success!" );
+   			// go through each tweet and push the text onto the messages array
+   			$.each(data, function(tweetNumber, tweet) {
+   				messages.push(tweet.text);
+   			})
    		})
+   		// called if the file was not found
    		.fail(function( jqxhr, textStatus, error ) {
+   			// print the error
 			var err = textStatus + ", " + error;
-			console.log( "Request Failed: " + err );
+			console.log( "JSON Request Failed: " + err );
 		});
 });
